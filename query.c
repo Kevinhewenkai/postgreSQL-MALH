b2911b6aad70a317f1d2399c2de10c3556073bd3
Wenkai <kevin.hewenkai@gmail.com>
2022-04-11 09:32:20 +1000

    try again

--- query.c
+++ query.c
@@ -151,7 +151,7 @@ Tuple getNextTuple(Query q)
             }
             q->curtup = q->curtup + strlen(tuple) + 1;
         }
-        gotoNextPage(q);
+        getNextTuple(q);
     }
         // else if (current page has overflow)
         //    move to overflow page
