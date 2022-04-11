1ada6b3a44bdd7de80852bc6d7e1d952bca5ef5e
Wenkai <kevin.hewenkai@gmail.com>
2022-04-11 15:36:11 +1000

    try again

--- query.c
+++ query.c
@@ -128,7 +128,7 @@ int gotoNextPage(Query q) {
         nextBucket = getLower(nextBucket, depth(q->rel)+1);
     }
 
-    // printf("next bucket: %d\n\n", nextBucket);
+     printf("next bucket: %d\n\n", nextBucket);
     // else will happen goto page 1,3,5,7, 7, 9, 18, 1, 3, 5, 7....
     if (nextBucket <= q->curpage) return 1;
     q->curpage = nextBucket;
