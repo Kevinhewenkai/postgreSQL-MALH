338187522ac36048da2dcf916c4982632bcd63a5
Kevinhewenkai <kevin.hewenkai@gmail.com>
2022-04-13 00:41:17 +1000

    goto pc and have some test

---
+++ testing/testing/query.c
@@ -0,0 +1,258 @@
+// query.c ... query scan functions
+// part of Multi-attribute Linear-hashed Files
+// Manage creating and using Query objects
+// Last modified by John Shepherd, July 2019
+
+#include "defs.h"
+#include "query.h"
+#include "reln.h"
+#include "tuple.h"
+#include "hash.h"
+
+// A suggestion ... you can change however you like
+
+struct QueryRep {
+	Reln    rel;       // need to remember Relation info
+	Bits    known;     // the hash value from MAH
+	Bits    unknown;   // the unknown bits from MAH
+	PageID  curpage;   // current page in scan
+	int     is_ovflow; // are we in the overflow pages?
+	Offset  curtup;    // offset of current tuple within page
+//    char*  curtup;    // offset of current tuple within page
+	//TODO
+    Offset curTupIndex;    // index for check Is there more tuple in page
+    PageID  curScanPage; // overflow page or data page
+    Tuple query;
+    Bits unknownOffset; // start with 0 while goto next bucket ut plus 1 and | bit in unknown
+    Bits checkAllBucket; // if checkAllBucket = 00011111111111(num(not 0 unknown)) then we have looped all buckets
+};
+
+// take a query string (e.g. "1234,?,abc,?")
+// set up a QueryRep object for the scan
+
+Query startQuery(Reln r, char *q)
+{
+//    char *chair = "chair";
+//    char *shoes = "shoes";
+//    char buf[MAXBITS + 1];
+//    Bits testHash = hash_any((unsigned char *)chair, strlen(chair));
+//    bitsString(testHash, buf);
+//    printf("hash of chair %s\n", buf);
+//    testHash = hash_any((unsigned char *)shoes, strlen(shoes));
+//    bitsString(testHash, buf);
+//    printf("hash of shoes %s\n", buf);
+	Query new = malloc(sizeof(struct QueryRep));
+	assert(new != NULL);
+	// TODO
+    char **attribs = malloc(nattrs(r) * sizeof(char *));
+    tupleVals(q, attribs);
+
+    ChVecItem *cv = chvec(r);
+    int numberOfUnknownBits = 0;
+    // loop each attribute
+    for (int i = 0; i < nattrs(r); i ++) {
+        // hash = 00101001010101
+        // cv= bits,attrib : bits,attrib ...
+        // hash == hash of current attrib
+        Bits hash = hash_any((unsigned char *)attribs[i], strlen(attribs[i]));
+//        printf("hash %d\n", hash);
+        // loop each cvItem in choice vector
+        for (int j = 0; j < MAXCHVEC; j++) {
+            // if cv's attrib = the attrib we are scanning,
+            if (cv[j].att == i) {
+//                printf("attr[i] %s, i = %d\n", attribs[i], i);
+                if (strcmp(attribs[i], "?") != 0) {
+                    // set known bits at position cv.bits where the given query attrib is not ?
+                    // get bits == cv.pos
+//                    printf("attr: %u bits %u attrib %s\n", cv[j].att, cv[j].bit, attribs[i]);
+//                    printf("bit is set: %d\n", bitIsSet(hash, cv[j].bit));
+//                    bitsString(new->known, buf);
+//                    printf("known: %s\n", buf);
+//                    bitsString(new->unknown,buf);
+//                    printf("unknown: %s\n", buf);
+                    if (bitIsSet(hash, cv[j].bit)) {
+                        new->known = setBit(new->known, j);
+                    } else {
+                        new->known = unsetBit(new->known, j);
+                    }
+               } else {
+                    new->unknown = setBit(new->unknown, j);
+                    numberOfUnknownBits++;
+               }
+           }
+        }
+    }
+    // form unknown bits from '?' attributes
+//    char buf[MAXCHVEC+1];
+//    bitsString(new->known, buf);
+//    printf("known: %s\n\n", buf);
+//    bitsString(new->unknown, buf);
+//    printf("unknown: %s\n\n", buf);
+//    printf("depth: %d\n\n", depth(r));
+    // TODO lecture linear hashing 4s
+    PageID pid = getLower(new->known, depth(r));
+    if (pid < splitp(r)) {
+        pid = getLower(new->known, depth(r)+1);
+    }
+
+    new->rel = r;
+    // printf("SETUP first page is %d\n", pid);
+    new->curpage = pid;
+    new->is_ovflow = 0;
+//    printf("Line80\n\n");
+//    printf("line 81\n\n");
+    new->curtup = 0;
+    new->curTupIndex = 0;
+    new->curScanPage = pid;
+    new->query = q;
+    new->unknownOffset = 0;
+
+    for (int i = 0; i < numberOfUnknownBits; i++) {
+//        new->checkAllBucket = new->checkAllBucket << 1;
+//        new->checkAllBucket = new->checkAllBucket | 1;
+        new->checkAllBucket++;
+    }
+	// compute PageID of first page
+	//   using known bits and first "unknown" value
+	// set all values in QueryRep object
+	return new;
+}
+
+int gotoNextPage(Query q) {
+    Bits nextBucket = q->known;
+//    printf("checkBucket: %d Offset: %d\n\n", q->checkAllBucket, q->unknownOffset);
+    if (q->unknownOffset >= q->checkAllBucket) {
+        // printf("return 1\n\n");
+        return 1;
+    }
+    q->unknownOffset += 1;
+    Bits tmp = q->unknownOffset;
+//    printf("offset: %d\n\n", q->unknownOffset);
+//    printf("known: %d\n\n", q->known);
+//    printf("unknown: %d\n\n", q->unknown);
+    for (int i = 0; i < MAXBITS; i++) {
+        if (bitIsSet(q->unknown, i)) {
+            nextBucket = nextBucket | ((tmp & 1) << i);
+            tmp = tmp >> 1;
+        }
+    }
+    Bits tmpBucket = nextBucket;
+    nextBucket = getLower(nextBucket, depth(q->rel));
+//    printf("sp: %d\n", splitp(q->rel));
+//    printf("q->curpage : %d\n", q->curpage);
+    if (q->curpage < npages(q->rel)) {
+//        printf("1111\n");
+        int d = depth(q->rel) + 1;
+//        printf("if statement depth = %d\n", d);
+//        char buf[MAXBITS+1];
+//        bitsString(nextBucket, buf);
+//        printf("buf : %s\n", buf);
+//        bitsString(q->unknownOffset, buf);
+//        printf("offset: %s\n", buf);
+        nextBucket = getLower(tmpBucket, d);
+    }
+
+//     printf("next bucket: %d\n\n", nextBucket);
+    // else will happen goto page 1,3,5,7, 7, 9, 18, 1, 3, 5, 7....
+    if (nextBucket <= q->curpage) return 1;
+    if (q->is_ovflow) {
+
+    }
+    q->curpage = nextBucket;
+    q->curScanPage = nextBucket;
+//    printf("line 113\n\n");
+    q->curtup = 0;
+    q->is_ovflow = 0;
+    q->curTupIndex = 0;
+    return 0;
+}
+
+// get next tuple during a scan
+
+Tuple getNextTuple(Query q)
+{
+	// Partial algorithm:
+    // if (more tuples in current page)
+    //    get next matching tuple from current page
+//    printf("start looping\n");
+    while (1) {
+        FILE *file = (q->is_ovflow) ? ovflowFile(q->rel) : dataFile(q->rel);
+//        printf("curPage: %d\n\n", q->curScanPage);
+//        printf("Is overflow: %d\n\n", q->is_ovflow);
+    //    printf("curTuple index: %d\n\n", q->curTupIndex);
+        Page page = getPage(file, q->curScanPage);
+//        printf("page have n tuple: %d\n\n", pageNTuples(page));
+//        printf("tuple: %s\n\n", tuple);
+        if (q->curTupIndex < pageNTuples(page)) {
+            while (q->curTupIndex < pageNTuples(page)) {
+                char *tuple = pageData(page);
+                // jump to the next tuple
+                tuple += q->curtup;
+//                printf("tuple: %s\n", tuple);
+                q->curTupIndex++;
+                if (tupleMatch(q->rel, tuple, q->query)) {
+                    q->curtup += tupLength(tuple) + 1;
+                    // move to the next tuple
+//                    printf("success, tuple: %s\n", tuple);
+                    return tuple;
+                }
+                q->curtup += tupLength(tuple) + 1;
+//                free(tuple);
+            }
+//            continue;
+        }
+            // else if (current page has overflow)
+            //    move to overflow page
+            //    grab first matching tuple from page
+        if (pageOvflow(page) != NO_PAGE) {
+//            printf("overflow!!\n\n");
+            q->curScanPage = pageOvflow(page);
+//            printf("NEXT OVERFLOW %d\n\n", q->curScanPage);
+            q->curTupIndex = 0;
+            q->is_ovflow = 1;
+            q->curtup = 0;
+            continue;
+        }
+            // else
+            //    move to "next" bucket
+            //    grab first matching tuple from data page
+            // endif
+            //     E.g. assuming only 8 bits, known bits = 00110001, unknown bits = 10000100
+            //     We need to fill two bits to make a complete bit-string; assume those two bits are 01
+            //     The complete pattern for the unknown bits is 00000100
+            //     Overlaying this on 00110001 gives 00110101 which is binary for 53 (I hope)
+            //     So you access page 53
+            //     There are three other bit patterns to fill the unknown bits 11, 10, 00 (as well as 01)
+        else {
+            int check = gotoNextPage(q);
+            if (check) {
+                // printf("check return NULL");
+                return NULL;
+            }
+//            printf("nextBucket!! Bucket number = %d\n\n", q->curpage);
+//            char buf[MAXCHVEC+1];
+//            bitsString(q->known, buf);
+//            printf("known: %s\n", buf);
+//            bitsString(q->unknown, buf);
+//            printf("unknown: %s\n", buf);
+//            bitsString(q->curpage, buf);
+//            printf("curPage %s\n", buf);
+//            bitsString(q->unknownOffset, buf);
+//            printf("unknownOffset %s\n", buf);
+//            printf("depth: %d\n", depth(q->rel));
+//            printf("\n");
+            //        printf("check %d\n\n", check);
+        }
+    // if (current page has no matching tuples)
+    //    go to next page (try again)
+    // endif
+    }
+//    printf("111111111\n");
+}
+
+// clean up a QueryRep object and associated data
+
+void closeQuery(Query q)
+{
+    free(q);
+}
---
+++ testing/testing/reln.c
@@ -0,0 +1,311 @@
+;// reln.c ... functions on Relations
+// part of Multi-attribute Linear-hashed Files
+// Last modified by John Shepherd, July 2019
+
+#include "defs.h"
+#include "reln.h"
+#include "page.h"
+#include "tuple.h"
+#include "chvec.h"
+#include "bits.h"
+#include "hash.h"
+
+#define HEADERSIZE (3*sizeof(Count)+sizeof(Offset))
+
+struct RelnRep {
+	Count  nattrs; // number of attributes
+	Count  depth;  // depth of main data file
+	Offset sp;     // split pointer
+    Count  npages; // number of main data pages
+    Count  ntups;  // total number of tuples
+	ChVec  cv;     // choice vector
+	char   mode;   // open for read/write
+	FILE  *info;   // handle on info file
+	FILE  *data;   // handle on data file
+	FILE  *ovflow; // handle on ovflow file
+};
+
+// create a new relation (three files)
+
+Status newRelation(char *name, Count nattrs, Count npages, Count d, char *cv)
+{
+    char fname[MAXFILENAME];
+	Reln r = malloc(sizeof(struct RelnRep));
+	r->nattrs = nattrs; r->depth = d; r->sp = 0;
+	r->npages = npages; r->ntups = 0; r->mode = 'w';
+	assert(r != NULL);
+	if (parseChVec(r, cv, r->cv) != OK) return ~OK;
+	sprintf(fname,"%s.info",name);
+	r->info = fopen(fname,"w");
+	assert(r->info != NULL);
+	sprintf(fname,"%s.data",name);
+	r->data = fopen(fname,"w");
+	assert(r->data != NULL);
+	sprintf(fname,"%s.ovflow",name);
+	r->ovflow = fopen(fname,"w");
+	assert(r->ovflow != NULL);
+	int i;
+	for (i = 0; i < npages; i++) addPage(r->data);
+	closeRelation(r);
+	return 0;
+}
+
+// check whether a relation already exists
+
+Bool existsRelation(char *name)
+{
+	char fname[MAXFILENAME];
+	sprintf(fname,"%s.info",name);
+	FILE *f = fopen(fname,"r");
+	if (f == NULL)
+		return FALSE;
+	else {
+		fclose(f);
+		return TRUE;
+	}
+}
+
+// set up a relation descriptor from relation name
+// open files, reads information from rel.info
+
+Reln openRelation(char *name, char *mode)
+{
+	Reln r;
+	r = malloc(sizeof(struct RelnRep));
+	assert(r != NULL);
+	char fname[MAXFILENAME];
+	sprintf(fname,"%s.info",name);
+	r->info = fopen(fname,mode);
+	assert(r->info != NULL);
+	sprintf(fname,"%s.data",name);
+	r->data = fopen(fname,mode);
+	assert(r->data != NULL);
+	sprintf(fname,"%s.ovflow",name);
+	r->ovflow = fopen(fname,mode);
+	assert(r->ovflow != NULL);
+	// Naughty: assumes Count and Offset are the same size
+	int n = fread(r, sizeof(Count), 5, r->info);
+	assert(n == 5);
+	n = fread(r->cv, sizeof(ChVecItem), MAXCHVEC, r->info);
+	assert(n == MAXCHVEC);
+	r->mode = (mode[0] == 'w' || mode[1] =='+') ? 'w' : 'r';
+	return r;
+}
+
+// release files and descriptor for an open relation
+// copy latest information to .info file
+
+void closeRelation(Reln r)
+{
+	// make sure updated global data is put in info
+	// Naughty: assumes Count and Offset are the same size
+	if (r->mode == 'w') {
+		fseek(r->info, 0, SEEK_SET);
+		// write out core relation info (#attr,#pages,d,sp)
+		int n = fwrite(r, sizeof(Count), 5, r->info);
+		assert(n == 5);
+		// write out choice vector
+		n = fwrite(r->cv, sizeof(ChVecItem), MAXCHVEC, r->info);
+		assert(n == MAXCHVEC);
+	}
+	fclose(r->info);
+	fclose(r->data);
+	fclose(r->ovflow);
+	free(r);
+}
+
+// insert a new tuple into a relation
+// returns index of bucket where inserted
+// - index always refers to a primary data page
+// - the actual insertion page may be either a data page or an overflow page
+// returns NO_PAGE if insert fails completely
+// TODO: include splitting and file expansion
+
+
+PageID addToRelationPage(Reln r, PageID p, Tuple t)
+{
+    Page pg = getPage(r->data,p);
+    if (addToPage(pg,t) == OK) {
+        putPage(r->data,p,pg);
+        return p;
+    }
+    // primary data page full
+    if (pageOvflow(pg) == NO_PAGE) {
+        // add first overflow page in chain
+        PageID newp = addPage(r->ovflow);
+        // append the overflow page to datapage
+        pageSetOvflow(pg,newp);
+        putPage(r->data,p,pg);
+        Page newpg = getPage(r->ovflow,newp);
+        // can't add to a new page; we have a problem
+        if (addToPage(newpg,t) != OK) return NO_PAGE;
+        putPage(r->ovflow,newp,newpg);
+        return p;
+    }
+    else {
+        // scan overflow chain until we find space
+        // worst case: add new ovflow page at end of chain
+        Page ovpg, prevpg = NULL;
+        PageID ovp, prevp = NO_PAGE;
+        ovp = pageOvflow(pg);
+        while (ovp != NO_PAGE) {
+            ovpg = getPage(r->ovflow, ovp);
+            if (addToPage(ovpg,t) != OK) {
+                prevp = ovp; prevpg = ovpg;
+                ovp = pageOvflow(ovpg);
+            }
+            else {
+                if (prevpg != NULL) free(prevpg);
+                putPage(r->ovflow,ovp,ovpg);
+                return p;
+            }
+        }
+        // all overflow pages are full; add another to chain
+        // at this point, there *must* be a prevpg
+        assert(prevpg != NULL);
+        // make new ovflow page
+        PageID newp = addPage(r->ovflow);
+        // insert tuple into new page
+        Page newpg = getPage(r->ovflow,newp);
+        if (addToPage(newpg,t) != OK) return NO_PAGE;
+        putPage(r->ovflow,newp,newpg);
+        // link to existing overflow chain
+        pageSetOvflow(prevpg,newp);
+        putPage(r->ovflow,prevp,prevpg);
+        return p;
+    }
+    return NO_PAGE;
+}
+
+// lecture linear hashing slide 11
+void spilt(Reln r) {
+    PageID pid = r->sp;
+    Page p = getPage(r->data, pid);
+    // add a new page the pid of new should be sp + 2^d
+    PageID newPageId = addPage(r->data);
+    r->npages++;
+    // cover all tuple in old pid
+    Page np = newPage();
+    putPage(r->data, pid, np);
+
+    // loop all tuples in datapage and overflow page
+    char *oldPageData = pageData(p);
+    for(int i = 0; i < pageNTuples(p); i++) {
+        printf("datapage data: %s\n\n", oldPageData);
+        Bits hash = tupleHash(r, oldPageData);
+        Bits low = getLower(hash, depth(r) + 1);
+        printf("new depth should be %d\n", (r->sp + (1<< depth(r))));
+        printf("now the pid is %d\n", pid);
+        printf("r->sp = %d, d = %d\n", r->sp, depth(r));
+        printf("low = %d\n",getLower(newPageId, depth(r) + 1));
+        if (low == getLower(newPageId, depth(r) + 1)) {
+            addToRelationPage(r, low, oldPageData);
+        } else {
+            addToRelationPage(r, pid, oldPageData);
+        }
+        oldPageData += strlen(oldPageData) + 1;
+    }
+    // go to overflow page
+    Page ovpg;
+    PageID ovp;
+    ovp = pageOvflow(p);
+    while (ovp != NO_PAGE) {
+        ovpg = getPage(r->ovflow, ovp);
+        printf("\n\nOverFlow Page ID: %d\n\n", ovp);
+        char *overflowData = pageData(ovpg);
+	    for (int i = 0; i <  pageNTuples(ovpg); i++) {
+            printf("overflow data: %s\n\n", overflowData);
+            Bits hash = tupleHash(r, overflowData);
+            Bits low = getLower(hash, depth(r) + 1);
+            if (low == getLower(newPageId, depth(r) + 1)) {
+                addToRelationPage(r, low, overflowData);
+            }
+            else {
+                addToRelationPage(r, pid, overflowData);
+            }
+            overflowData += strlen(overflowData) + 1;
+        }
+        ovp = pageOvflow(ovpg);
+    }
+    r->sp++;
+    // if sp == 2^d
+    if (r->sp == 1<< depth(r)) {
+        r->depth++;
+        r->sp = 0;
+    }
+
+}
+
+PageID addToRelation(Reln r, Tuple t)
+{
+	Bits h, p;
+	// char buf[MAXBITS+1];
+	h = tupleHash(r,t);
+    printf("DONT SPLIT\n");
+	if (r->depth == 0)
+		p = 1;
+	else {
+		p = getLower(h, r->depth);
+		if (p < r->sp) p = getLower(h, r->depth+1);
+	}
+	// bitsString(h,buf); printf("hash = %s\n",buf);
+	// bitsString(p,buf); printf("page = %s\n",buf);
+    // success
+    PageID result = addToRelationPage(r, p, t);
+    if (result != NO_PAGE) {
+        r->ntups++;
+
+        // // do the split if needed
+        Count c = 1024/(10 * r->nattrs);
+        if ((r->ntups) % c == 0) {
+            spilt(r);
+        }
+    }
+
+    return result;
+}
+
+// external interfaces for Reln data
+
+FILE *dataFile(Reln r) { return r->data; }
+FILE *ovflowFile(Reln r) { return r->ovflow; }
+Count nattrs(Reln r) { return r->nattrs; }
+Count npages(Reln r) { return r->npages; }
+Count ntuples(Reln r) { return r->ntups; }
+Count depth(Reln r)  { return r->depth; }
+Count splitp(Reln r) { return r->sp; }
+ChVecItem *chvec(Reln r)  { return r->cv; }
+
+
+// displays info about open Reln
+
+void relationStats(Reln r)
+{
+	printf("Global Info:\n");
+	printf("#attrs:%d  #pages:%d  #tuples:%d  d:%d  sp:%d\n",
+	       r->nattrs, r->npages, r->ntups, r->depth, r->sp);
+	printf("Choice vector\n");
+	printChVec(r->cv);
+	printf("Bucket Info:\n");
+	printf("%-4s %s\n","#","Info on pages in bucket");
+	printf("%-4s %s\n","","(pageID,#tuples,freebytes,ovflow)");
+	for (Offset pid = 0; pid < r->npages; pid++) {
+		printf("[%2d]  ",pid);
+		Page p = getPage(r->data, pid);
+		Count ntups = pageNTuples(p);
+		Count space = pageFreeSpace(p);
+		Offset ovid = pageOvflow(p);
+		printf("(d%d,%d,%d,%d)",pid,ntups,space,ovid);
+		free(p);
+		while (ovid != NO_PAGE) {
+			Offset curid = ovid;
+			p = getPage(r->ovflow, ovid);
+			ntups = pageNTuples(p);
+			space = pageFreeSpace(p);
+			ovid = pageOvflow(p);
+			printf(" -> (ov%d,%d,%d,%d)",curid,ntups,space,ovid);
+			free(p);
+		}
+		putchar('\n');
+	}
+}
